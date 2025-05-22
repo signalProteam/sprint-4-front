1485895ce966cf7b74d13e433fe64a264f1c691d
rangelbjordao <96200914+rangelbjordao@users.noreply.github.com>
2025-05-22 17:49:14 -0300

    Update api.ts

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE = process.env.NEXT_PUBLIC_API_BASE;
+export const API_BASE = 'https://ccr-alertas-api.up.railway.app';
 
 export const getHeaders = () => ({
     "Content-Type": "application/json",
