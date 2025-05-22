2fc986b120403b5b6c9d8a6ff0924d8209ee970e
rangelbjordao <96200914+rangelbjordao@users.noreply.github.com>
2025-05-22 17:48:41 -0300

    Update api.ts

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE = process.env.NEXT_PUBLIC_API_BASE || 'http://localhost:8080';
+export const API_BASE = process.env.NEXT_PUBLIC_API_BASE;
 
 export const getHeaders = () => ({
     "Content-Type": "application/json",
