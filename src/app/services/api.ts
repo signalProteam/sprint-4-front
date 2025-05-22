cc98003cfe5edcb8c7847a012db3bc512c5c9408
rangelbjordao <96200914+rangelbjordao@users.noreply.github.com>
2025-05-22 19:55:04 -0300

    Update api.ts

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -2,7 +2,7 @@ const isBrowser = typeof window !== "undefined";
 
 export const API_BASE = isBrowser
     ? process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080"
-    : "http://localhost:8080"; // Fallback no lado do servidor (SSR)
+    : "http://localhost:8080";
 
 export const getHeaders = () => ({
     "Content-Type": "application/json",
