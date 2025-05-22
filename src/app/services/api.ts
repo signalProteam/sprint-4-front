13e7d0c982f43d09694822396fdd369c12eb4d73
rangelbjordao <96200914+rangelbjordao@users.noreply.github.com>
2025-05-22 17:37:57 -0300

    Update api.ts

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,4 +1,6 @@
-export const API_BASE = 'http://localhost:8080';
+export const API_BASE = process.env.NODE_ENV === 'production'
+    ? 'https://ccr-alertas-api.up.railway.app'
+    : 'http://localhost:8080';
 
 export const getHeaders = () => ({
     "Content-Type": "application/json",
