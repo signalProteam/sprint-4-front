9f4c7a6e90dfde06cb18a0d2bc728d2c630d9cc1
rangelbjordao <96200914+rangelbjordao@users.noreply.github.com>
2025-05-22 19:51:24 -0300

    Update api.ts

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,4 +1,8 @@
-export const API_BASE = 'https://ccr-alertas-api.up.railway.app';
+const isBrowser = typeof window !== "undefined";
+
+export const API_BASE = isBrowser
+    ? process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080"
+    : "http://localhost:8080"; // Fallback no lado do servidor (SSR)
 
 export const getHeaders = () => ({
     "Content-Type": "application/json",
