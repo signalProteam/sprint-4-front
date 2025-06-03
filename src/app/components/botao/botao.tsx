503be5f06c133dd24278b6534dee582717a9b5d7
rangelbjordao <96200914+rangelbjordao@users.noreply.github.com>
2025-06-03 10:48:24 -0300

    Update botao.tsx

--- src/app/components/botao/botao.tsx
+++ src/app/components/botao/botao.tsx
@@ -15,7 +15,7 @@ const Botao: React.FC<BotaoProps> = ({
         <>
             <button type={type} className={`bg-red-700 text-white border border-gray-800 rounded-md px-5 py-3 m-2 text-base cursor-pointer w-64 transition-colors duration-300 hover:bg-red-900 hover:border-gray-600 ${carregando ? "opacity-50 cursor-not-allowed" : ""}`} disabled={carregando}>
                 {carregando ? "Carregando..." : texto}
-            </button >
+            </button>
         </>
     )
 }
